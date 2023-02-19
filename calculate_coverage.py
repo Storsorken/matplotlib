dc72ce0bfa2655d0eee8963170b9d9c6e21f6bdc
patrik kinnunen <patkin@kth.se>
2023-02-19 14:23:17 +0100

    feat: #12 script to read from flag_arrays.yml, can clear file as well resolves the issue.

---
+++ calculate_coverage.py
@@ -0,0 +1,26 @@
+import yaml
+import numpy as np
+
+with open("flag_arrays.yml") as fin:
+        data = yaml.load(fin, Loader=yaml.FullLoader)
+boxplot_array = np.array(data["BOXPLOT_ARRAY"])
+eventplot_array = np.array(data["EVENTPLOT_ARRAY"])
+hist_array = np.array(data["HIST_ARRAY"])
+make_image_array = np.array(data["MAKE_IMAGE_ARRAY"])
+spectral_helper_array = np.array(data["SPECTRAL_HELPER_ARRAY"])
+
+print("BOXPLOT coverage: ", boxplot_array.sum()*100/len(boxplot_array), "%")
+print("EVENTPLOT coverage: ", eventplot_array.sum()*100/len(eventplot_array), "%")
+print("HIST coverage: ", hist_array.sum()*100/len(hist_array), "%")
+print("MAKE_IMAGE coverage: ", make_image_array.sum()*100/len(make_image_array), "%")
+print("SPECTRAL_HELPER coverage: ", spectral_helper_array.sum()*100/len(spectral_helper_array), "%")
+
+reset = input('\nClear flag_arrays.yml file? (Y/N):')
+if reset == 'Y':
+    data["BOXPLOT_ARRAY"] = []
+    data["EVENTPLOT_ARRAY"]= []
+    data["HIST_ARRAY"] = []
+    data["MAKE_IMAGE_ARRAY"] = []
+    data["SPECTRAL_HELPER_ARRAY"] = []
+    with open("flag_arrays.yml", "w") as f:
+        yaml.dump(data, f)
